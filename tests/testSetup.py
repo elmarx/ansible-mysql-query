29404a87e42caa39932194e1c013f7dc2570ff27
Elmar Athmer <elmar@athmer.org>
2015-08-05 23:30:40 +0200

    add python test to run ansible

--- tests/testSetup.py
+++ tests/testSetup.py
@@ -1,6 +1,19 @@
 import unittest
+import ansible.runner
+from ansible.inventory import Inventory
 
-class MyTest(unittest.TestCase):
 
-    def test_something(self):
+class SetupTest(unittest.TestCase):
+    def test_ping(self):
         self.assertEqual(True, True)
+
+        runner = ansible.runner.Runner(
+            module_name='ping',
+            module_args='',
+            forks=10,
+            inventory=Inventory(['localhost']),
+            transport='local'
+        )
+        datastructure = runner.run()
+        self.assertEqual(len(datastructure['contacted']), 1)
+        self.assertEqual(len(datastructure['dark']), 0)
