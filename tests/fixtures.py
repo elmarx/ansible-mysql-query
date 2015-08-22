402aab59c6d6bb228cb49337bd1155c2e03950f3
Elmar Athmer <elmar@athmer.org>
2015-08-23 00:48:52 +0200

    add tests for multiple identifiers and values

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -30,11 +30,38 @@ class Fixture:
                 ) ENGINE=InnoDB;""")
         cur.close()
 
+    def create_multicolumn_example(self):
+        cur = self.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS `multicolumn_example` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                  `identifier1` varchar(128) NOT NULL UNIQUE,
+                  `identifier2` int(11) NOT NULL UNIQUE,
+                  `identifier3` varchar(128) NOT NULL UNIQUE,
+                  `value1` int(11) NOT NULL,
+                  `value2` varchar(255) NOT NULL,
+                  `value3` text
+                ) ENGINE=InnoDB;""")
+        cur.close()
+
     def insert_into_key_value_example(self, key, value):
         cur = self.cursor()
         cur.execute('insert into `key_value_example` values (DEFAULT, %s, %s);', (key, value))
         self._connection.commit()
         cur.close()
+
+    def insert_into_multicolumn_example(self, ids, vals):
+        cur = self.cursor()
+        cur.execute("insert into `multicolumn_example` values (DEFAULT, '%s', %s, '%s', %s, '%s', '%s');" %
+                    tuple(ids + vals))
+        self._connection.commit()
+        cur.close()
+
+    def count_multicolumn_example(self):
+        cur = self.cursor()
+        cur.execute('select COUNT(*) from `multicolumn_example`')
+        (result,) = cur.fetchone()
+        cur.close()
+        return result
 
     def count_key_value_example(self):
         cur = self.cursor()
---
+++ tests/testMysqlQueryMultiCheck.py
@@ -0,0 +1,44 @@
+import unittest
+
+from tests import utils
+from tests.fixtures import Fixture
+
+class MysqlQueryMultiCheckTest(unittest.TestCase):
+    def setUp(self):
+        self.f = Fixture()
+        self.f.create_database()
+        self.f.create_multicolumn_example()
+
+    def tearDown(self):
+        self.f.close()
+
+    def testInsertRequired(self):
+        args = dict(
+            login_user='root',
+            name='ansible-mysql-query-test',
+            table='multicolumn_example',
+            identifiers=dict(identifier1='elmar@athmer.org', identifier2='4', identifier3='testInsert'),
+            values=dict(value1='8', value2='admin', value3="made up"),
+        )
+
+        result = utils.ansible_run(args)
+        self.assertTrue(result['changed'], 'a required change is detected')
+        self.assertRegexpMatches(result['msg'], 'insert')
+        self.assertEquals(self.f.count_multicolumn_example(), 0, 'no row has been inserted in check-mode')
+
+    def testNoChangeRequired(self):
+        # insert a row that does not need to be updated
+        self.f.insert_into_multicolumn_example(['elmar@athmer.org', 4, 'testNoChangeRequired'], [8, 'admin', 'made up'])
+
+        args = dict(
+            login_user='root',
+            name='ansible-mysql-query-test',
+            table='multicolumn_example',
+            identifiers=dict(identifier1='elmar@athmer.org', identifier2='4', identifier3='testNoChangeRequired'),
+            values={'value1': '8', 'value2': 'admin', 'value3': "made up"},
+        )
+
+        result = utils.ansible_run(args)
+        self.assertIn('changed', result)
+        self.assertFalse(result['changed'], 'no changed required is detected')
+        self.assertEquals(self.f.count_multicolumn_example(), 1, 'no additional row has been inserted in check-mode')
