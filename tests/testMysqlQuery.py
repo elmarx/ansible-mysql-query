a8023e6d2fac61701f91a768eeeb9d2107f26646
Elmar Athmer <elmar@athmer.org>
2015-08-22 23:05:05 +0200

    reimplement check-mode for all cases

--- tests/testMysqlQuery.py
+++ tests/testMysqlQuery.py
@@ -12,20 +12,18 @@ class MysqlQueryTest(unittest.TestCase):
     def tearDown(self):
         self.f.close()
 
-    @unittest.skip("not yet implemented after refactoring")
     def testCheckInsertRequired(self):
         args = dict(
             login_user='root',
             name='ansible-mysql-query-test',
             table='key_value_example',
-            identifier_column='name',
-            identifier='myKey',
-            value_column='value',
-            value='myValue',
+            identifiers='name=testCheckInsertRequired_myKey',
+            values='value=42',
         )
 
         result = utils.ansible_run(args)
-        self.assertTrue(result['contacted']['localhost']['changed'], 'a required change is detected')
+        self.assertTrue(result['changed'], 'a required change is detected')
+        self.assertRegexpMatches(result['msg'], 'insert')
         self.assertEquals(self.f.count_key_value_example(), 0, 'no row has been inserted in check-mode')
 
     def testFailForEmptyValues(self):
@@ -37,19 +35,39 @@ class MysqlQueryTest(unittest.TestCase):
 
         result = utils.ansible_run(args)
         self.assertTrue(result['failed'])
-        self.assertEqual(result['msg'], 'missing required arguments: values')
+        self.assertEqual(result['msg'], 'missing required arguments: identifiers')
 
     def testCheckNoChangeRequired(self):
         # insert a row that does not need to be updated
-        self.f.insert_into_key_value_example('myKey', 42)
+        self.f.insert_into_key_value_example('testCheckNoChangeRequired_myKey', 42)
 
         args = dict(
             login_user='root',
             name='ansible-mysql-query-test',
             table='key_value_example',
-            values='name=myKey value=42',
+            identifiers='name=testCheckNoChangeRequired_myKey',
+            values='value=42',
         )
 
         result = utils.ansible_run(args)
+        self.assertIn('changed', result)
         self.assertFalse(result['changed'], 'no changed required is detected')
+        self.assertEquals(self.f.count_key_value_example(), 1, 'no additional row has been inserted in check-mode')
+
+    def testCheckUpdateRequired(self):
+        # insert a row that does not need to be updated
+        self.f.insert_into_key_value_example('testCheckUpdateRequired_myKey', 4)
+
+        args = dict(
+            login_user='root',
+            name='ansible-mysql-query-test',
+            table='key_value_example',
+            identifiers='name=testCheckUpdateRequired_myKey',
+            values='value=8',
+        )
+
+        result = utils.ansible_run(args)
+        self.assertIn('changed', result)
+        self.assertTrue(result['changed'], 'a change (update) required is detected')
+        self.assertRegexpMatches(result['msg'], 'update')
         self.assertEquals(self.f.count_key_value_example(), 1, 'no additional row has been inserted in check-mode')
