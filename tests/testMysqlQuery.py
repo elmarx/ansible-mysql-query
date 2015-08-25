5d3f7b8fada98ebe658fd31914a876950ded7348
Elmar Athmer <elmar@athmer.org>
2015-08-25 20:32:19 +0200

    fail reliably for missing db tables

--- tests/testMysqlQuery.py
+++ tests/testMysqlQuery.py
@@ -1,9 +1,12 @@
 import unittest
-
 from tests import utils
-from tests.fixtures import Fixture
+from tests.fixtures import MYSQL_CONNECTION_PARAMS
 
-class MysqlQueryCheck(unittest.TestCase):
+
+class MysqlQueryTest(unittest.TestCase):
+    """
+    general robustness/configuration tests
+    """
     def testFailForEmptyValues(self):
         args = dict(
             login_user='root',
@@ -14,3 +17,18 @@ class MysqlQueryCheck(unittest.TestCase):
         result = utils.ansible_run(args)
         self.assertTrue(result['failed'])
         self.assertEqual(result['msg'], 'missing required arguments: identifiers')
+
+    def testFailForTableMissing(self):
+        args = dict(
+            login_user=MYSQL_CONNECTION_PARAMS['user'],
+            name=MYSQL_CONNECTION_PARAMS['db'],
+            login_password=MYSQL_CONNECTION_PARAMS['passwd'],
+            login_host=MYSQL_CONNECTION_PARAMS['host'],
+            table='does_not_exist_table',
+            identifiers=dict(not_relevant='bogus'),
+            values=dict(not_relevant2='bogus'),
+        )
+
+        result = utils.ansible_run(args)
+        self.assertTrue(result['failed'])
+        self.assertEqual(result['msg'], 'No such table `does_not_exist_table`')
